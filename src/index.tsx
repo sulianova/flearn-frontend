107833aefaa1b7cbf5e6fd354b6d0d45e59ee1b2
Vladimir Fyodorov <vfyodorov@nes.ru>
2023-05-19 23:00:55 +0600

    add css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'styles/index.scss';
 import './index.css';
 
 const root = ReactDOM.createRoot(
