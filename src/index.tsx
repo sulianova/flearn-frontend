bffbe3a368b3657e3db6e3a8c5dc494d403212d3
Vladimir Fyodorov <vfyodorov@nes.ru>
2023-05-21 16:36:39 +0600

    move app to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from './routes';
 
 import 'styles/index.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const router = createBrowserRouter(routes);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
