c8e58d4f390c93439bdd05ccc1921c6cf1f1c7d7
Vladimir Fyodorov <vfyodorov@nes.ru>
2023-07-10 21:10:48 +0600

    move URLSections

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,26 @@
-import { URLSections, TRouteConfig } from 'types';
+import { TRouteConfig } from 'types';
 
 import Catalogue from 'pages/Catalogue';
 import Course from 'pages/Course/Landing/Course';
 import SubRoutesCourse from 'pages/Course/routes';
 import FreeZone from 'pages/FreeZone';
 import SubRoutesMy from 'pages/My/routes';
+
+export const URLSections = {
+    FreeZone: { index: '/' },
+    Catalogue: { index: '/catalogue' },
+    Course: {
+        index: '/course',
+        Lessons: { index: '/course/lessons' },
+        Lesson: { index: '/course/lesson' },
+        Homework: { index: '/course/homework' },
+    },
+    My: {
+        index: '/my',
+        Profile: { index: '/my/profile' },
+        Settings: { index: '/my/settings' },
+    },
+} as const;
 
 const routes: TRouteConfig[] = [
     {
--- src/types/route.ts
+++ src/types/route.ts
@@ -1,20 +1,5 @@
 import { RouteObject } from 'react-router-dom';
-
-export const URLSections = {
-    FreeZone: { index: '/' },
-    Catalogue: { index: '/catalogue' },
-    Course: {
-        index: '/course',
-        Lessons: { index: '/course/lessons' },
-        Lesson: { index: '/course/lesson' },
-        Homework: { index: '/course/homework' },
-    },
-    My: {
-        index: '/my',
-        Profile: { index: '/my/profile' },
-        Settings: { index: '/my/settings' },
-    },
-} as const;
+import { URLSections } from 'routes';
 
 type TGetObjectValues<T extends {}, Keys extends keyof T = keyof T> =
     Keys extends Keys ?
