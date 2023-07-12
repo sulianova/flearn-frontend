c7391c0dfab7fd2dbedb4aea0385c6bcc3181d1c
sulianova <ulianova.sofia@gmail.com>
2023-07-12 17:56:27 +0600

    fix scss in landing ProgramIntro

--- src/pages/Course/Landing/ProgramIntro/ProgramIntro.tsx
+++ src/pages/Course/Landing/ProgramIntro/ProgramIntro.tsx
@@ -14,13 +14,13 @@ function ProgramIntro(props: IProps) {
     <div className={classes._} id='program-intro'>
       <div className={classes.inner}>
         <div className={classes.categories}>
-          <div className={classes['categories-item'] + ' s-text-18'}>{format(props.data.startDate, props.data.durationWeeks)}</div>
-          <div className={classes['categories-item'] + ' s-text-18'}>{props.data.feild}</div>
+          <div className={classes.categoriesItem + ' s-text-18'}>{format(props.data.startDate, props.data.durationWeeks)}</div>
+          <div className={classes.categoriesItem + ' s-text-18'}>{props.data.feild}</div>
         </div>
         <h1 className={classes.title + ' s-text-88'}>{props.data.title}</h1>
         <div className={classes.block + ' s-text-24'}>{props.data.description}</div>
-        <div className={classes.actions}><a className={classes['actions-btn'] + ' s-text-24'} href='#decision-form'>Записаться</a>
-          <div className={classes['actions-discount'] + ' s-text-24'} >-30% до 30 апреля</div>
+        <div className={classes.actions}><a className={classes.actionsBtn + ' s-text-24'} href='#decision-form'>Записаться</a>
+          <div className={classes.actionsDiscount + ' s-text-24'} >-30% до 30 апреля</div>
         </div>
         <div className={classes.cover}><img data-v-desktop alt='Иллюстрация' src={DrawFree}/></div>
       </div>
