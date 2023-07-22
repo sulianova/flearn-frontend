bda79d88e415b3faf7b4c9fe886ea06d1289313b
Vladimir Fyodorov <vfyodorov@nes.ru>
2023-07-22 15:42:14 +0600

    lint

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -4,5 +4,5 @@ import { createReducer } from './utils';
 import { IObject, TStateName } from 'types';
 
 export default combineReducers({
-    course: createReducer('course')
+    course: createReducer('course'),
 } as { [key in TStateName]: () => IObject });
