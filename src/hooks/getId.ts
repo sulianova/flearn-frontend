ff3d7789a872ee61ded629a91c2b9a778a147985
Vladimir Fyodorov <vfyodorov@nes.ru>
2023-07-28 20:52:37 +0600

    add id hooks + fix other hooks

---
+++ src/hooks/getId.ts
@@ -0,0 +1,10 @@
+import { v4 } from 'uuid';
+
+import { useConst } from 'hooks';
+
+export function useGetId() {
+    return useConst(() => {
+        const guid = v4();
+        return (prefix: string | number) => `${prefix}-${guid}`;
+    });
+}
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,5 @@
+export * from './const';
 export * from './fetch';
+export * from './getId';
+export * from './guid';
+export * from './memoize';
