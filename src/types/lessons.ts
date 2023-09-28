10b081156fc15144dc94dd55061401d4563c0270
sulianova <ulianova.sofia@gmail.com>
2023-09-28 09:50:58 +0200

    v0

--- src/types/lessons.ts
+++ src/types/lessons.ts
@@ -1,15 +1,26 @@
 import { ICourseInfo } from './course';
 
+export interface ILessonsState {
+  lessonsInfo: ILessonInfo[]
+  courseInfo: ICourseInfo
+}
+
+export interface ILessonInfoDB {
+  title: string
+  lectureLink?: string
+  homeworkLink?: string
+  webinarLink?: string
+  resultsLink?: string
+  week: number
+}
+
 export interface ILessonInfo {
   title: string
   lectureLink?: string
   homeworkLink?: string
   webinarLink?: string
   resultsLink?: string
-  dates: string
-}
-
-export interface ILessonsState {
-  lessonsInfo: ILessonInfo[]
-  courseInfo: ICourseInfo
+  week: number
+  startDate: Date
+  endDate: Date
 }
