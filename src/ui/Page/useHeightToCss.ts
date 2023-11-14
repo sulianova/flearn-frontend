bffc0110acecdf28d90760cd307b920ee7ce9547
Vladimir Fyodorov <vfyodorov@nes.ru>
2023-11-14 07:49:21 +0100

    fix edit bar

--- src/ui/Page/useHeightToCss.ts
+++ src/ui/Page/useHeightToCss.ts
@@ -1,24 +1,26 @@
 
 import { useEffect, useRef } from 'react';
+import { envService } from 'services';
 
 export default function useHeightToCss() {
   const ref = useRef<HTMLDivElement>(null);
 
-  useEffect(
-    () => {
-      if (ref.current) {
-        const page = ref.current;
-        const observer = new ResizeObserver(() => {
-          const height = page.clientHeight;
-          page.style.setProperty('--page-height', `${height}px`);
-        });
+  useEffect(() => {
+    if (envService.dataMode !== 'EDIT') {
+      return;
+    }
 
-        observer.observe(page);
-        return () => observer.disconnect();
-      }
-    },
-    []
-  );
+    if (ref.current) {
+      const page = ref.current;
+      const observer = new ResizeObserver(() => {
+        const height = page.clientHeight;
+        page.style.setProperty('--page-height', `${height}px`);
+      });
+
+      observer.observe(page);
+      return () => observer.disconnect();
+    }
+  },[]);
 
   return ref;
 }
