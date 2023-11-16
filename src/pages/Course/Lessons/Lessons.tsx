8da7a15a401eb4400f0eff89b765946f52e3f7f9
sulianova <ulianova.sofia@gmail.com>
2023-11-16 13:11:52 +0100

    add data to lesson

--- src/pages/Course/Lessons/Lessons.tsx
+++ src/pages/Course/Lessons/Lessons.tsx
@@ -117,7 +117,7 @@ function Lessons({ courseState, lessonsState, authedUserId }: IConnectedProps) {
     <div className={classesHeader.title + ' s-text-56'}>{courseState.data.title}</div>
       <div className={classesHeader.subTitle + ' s-text-24'}>{t('subTitle')}</div>
       <div className={classesHeader.links}>
-        <a className='key-link  s-text-18'>{t('linksAbout')}</a>
+        {/* <a className='key-link  s-text-18'>{t('linksAbout')}</a> */}
         <a className='key-link  s-text-18'>{t('linksTelegram')}</a>
       </div>
     </div>
