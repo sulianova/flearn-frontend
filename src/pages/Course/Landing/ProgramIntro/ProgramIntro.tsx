98108871a22896d44dc10613b286671d071f00a6
Vladimir Fyodorov <vfyodorov@nes.ru>
2023-12-03 11:50:22 +0100

    fix start date in Intro

--- src/pages/Course/Landing/ProgramIntro/ProgramIntro.tsx
+++ src/pages/Course/Landing/ProgramIntro/ProgramIntro.tsx
@@ -1,24 +1,34 @@
 import { formatI18nT, i18n } from 'shared';
+import { formatDate } from 'utils';
+
 import Image from 'ui/Img/Img';
-import Link from 'ui/Link/Link';
+
 import classes from './ProgramIntro.module.scss';
 
 import type { ICourseData } from 'types';
 
 export default ProgramIntro;
 
+const t = formatI18nT('courseLanding.programIntro');
+
 interface IProps {
   data: ICourseData
 }
 
 function ProgramIntro(props: IProps) {
+  const { startDate, durationWeeks } = props.data;
 
   return (
     <div className={classes._} id='program-intro'>
       <div className={classes.inner}>
         <div className={classes.info}>
           <div className={classes.categories}>
-            <div className={classes.categoriesItem + ' s-text-18'}>{formatCourseDate(props.data.startDate, props.data.durationWeeks)}</div>
+            <div className={classes.categoriesItem + ' s-text-18'}>
+              {t('datesInfoLabel', { 
+                startDate: formatDate(startDate, { timeZone: 'Europe/Moscow' }),
+                week: i18n.t('week', { count: durationWeeks }),
+                durationWeeks,
+              })}</div>
             <div className={classes.categoriesItem + ' s-text-18'}>{props.data.feild}</div>
           </div>
           <h1 className={classes.title + ' s-text-88'}>{props.data.title}</h1>
@@ -31,12 +41,4 @@ function ProgramIntro(props: IProps) {
       </div>
     </div>
   );
-}
-
-function formatCourseDate(startDate: Date, durationWeeks: number) {
-  const dateStr = startDate.toLocaleDateString(
-    ['ru-RU'],
-    { month: 'long', day: 'numeric' }
-  );
-  return `${dateStr} | ${durationWeeks} ` + i18n.t('week', { count: durationWeeks });
 }
