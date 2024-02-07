0894e018cc65ec5f8d16059e4a65ffeb32b06748
Vladimir Fyodorov <vfyodorov@nes.ru>
2024-02-07 17:21:01 +0100

    WIP

--- src/services/email.service/index.ts
+++ src/services/email.service/index.ts
@@ -1,17 +1,19 @@
 import { firebaseService } from "services/firebase.service";
 import { ECollections } from "types";
 import { v4 } from "uuid";
+import type { TEmail, TContact } from "./types";
 
 class EmailService {
   public async sendEmail(emailTo: string) {
-    const id = v4();
+    const id = v4().slice(0, 4);
     const email = {
       to: [{
         email: emailTo,
         name: 'Vladimir Fyodorov',
       }],
       from: {
-        email: 'ulianova.sofia@gmail.com',
+        email: 'MS_aXZ7Sh@flearn.net',
+        // email: 'info@flearn.net',
         name: 'Sofi',
       },
       subject: 'Hello from Firebase!',
@@ -20,6 +22,21 @@ class EmailService {
     };
     console.log('try to send email', { email, id });
     await firebaseService.setDoc(ECollections.EmailCollection, id, email);
+  }
+
+  private senderContact: TContact = {
+    email: 'info@flearn.net',
+    name: 'Sofi',
+  };
+
+  private getOrderEmail(props: { to: TContact, orderId: string }): TEmail {
+    const { to, orderId } = props;
+    return {
+      to: [to],
+      from: this.senderContact,
+      subject: 'Order created',
+      html: `<p>Your order with id ${orderId} has been created.</p>`,
+    };  
   }
 }
 
---
+++ src/services/email.service/types.ts
@@ -0,0 +1,11 @@
+export type TContact = {
+  email: string
+  name?: string
+}
+
+export type TEmail = {
+  to: TContact[],
+  from: TContact,
+  subject: string,
+  html: string,
+}
