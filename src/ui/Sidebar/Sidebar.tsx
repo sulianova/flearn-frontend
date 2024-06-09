d7c58236a3573fcfc011f0b6eeadb37be9855c5b
sulianova <ulianova.sofia@gmail.com>
2024-06-09 18:18:00 +0200

    Sidebar user popup

--- src/ui/Sidebar/Sidebar.tsx
+++ src/ui/Sidebar/Sidebar.tsx
@@ -1,20 +1,26 @@
+import classnames from 'classnames/bind';
 import { useState } from 'react';
 import { useParams } from 'react-router';
 
+import { userService } from 'services/user.service';
+
 import Portfolio from 'assets/images/Svg/Portfolio';
 import LessonsPopup from 'components/LessonsPopup/LessonsPopup';
-import UserPopup from 'components/UserPopup/UserPopup';
-import classnames from 'classnames/bind';
 import Link from 'ui/Link/Link';
 
+import UserPopup from './UserPopup/UserPopup';
 import classes from './Sidebar.module.scss';
+
 const cx = classnames.bind(classes);
 
 export default Sidebar;
 
 function Sidebar() {
   const { courseId, lessonId } = useParams();
+  const authedUser = userService.useAuthedUser();
   const [lessonsPopupVisible, setLessonsPopupVisible] = useState(false);
+  const [userPopupVisible, setUserPopupVisible] = useState(false);
+
   return (
     <>
       <div className={classes.__}>
@@ -68,11 +74,17 @@ function Sidebar() {
                 </div>
               </span>
             </li>
-            <li className={cx({ item: true, open: false })}>
-              <UserPopup/>
+            <li className={cx({ item: true, open: userPopupVisible })}>
+              {authedUser && userPopupVisible && (
+                <UserPopup
+                  user={authedUser}
+                  close={() => setUserPopupVisible(false)}
+                />
+              )}
               <span className={classes.iconWrapper}>
               <div
                   className={classes.icon}
+                  onClick={() => setUserPopupVisible(!userPopupVisible)}
                 >
                   <Portfolio/>
                 </div>
--- src/ui/Sidebar/UserPopup/UserPopup.tsx
+++ src/ui/Sidebar/UserPopup/UserPopup.tsx
@@ -1,19 +1,25 @@
 import classnames from 'classnames/bind';
 import classes from './UserPopup.module.scss';
 import Portfolio from 'assets/images/Svg/Portfolio';
+import { IUserData } from 'services/user.service';
+import { authService } from 'services';
 
 const cx = classnames.bind(classes);
 
+interface IProps {
+  user: IUserData
+  close: () => void
+}
 
- export default function UserPopup() {
+export default function UserPopup({ user, close }: IProps) {
   return (
     <>
-      {/* <div className={classes.overlay}></div> */}
-      <div className={cx({ popup: true, animated: true, inverseAnimated: true, open: false })}>
+      <div className={classes.overlay} onClick={close}></div>
+      <div className={cx({ popup: true, animated: true, inverseAnimated: true, open: true })}>
         <div className={classes.itemsGroup}>
          <div className={classes.item}>
-          <div className={classes.itemTitle + ' s-text-16'}>София Ульянова</div>
-          <div className={classes.itemSubtitle + ' s-text-14'}>ulianova.sofia@gmail.com</div>
+          <div className={classes.itemTitle + ' s-text-16'}>{user.displayName}</div>
+          <div className={classes.itemSubtitle + ' s-text-14'}>{user.email}</div>
           </div>
         </div>
         <div className={classes.itemsGroup}>
@@ -26,7 +32,7 @@ const cx = classnames.bind(classes);
             </div>
           </div>
           <div className={cx({ item: true, itemHoverable: true }) + ' s-text-16'}>
-            <div className={classes.itemTitle}>Выйти из профиля</div>
+            <div className={classes.itemTitle} onClick={() => authService.logout()}>Выйти из профиля</div>
           </div>
         </div>
       </div>
