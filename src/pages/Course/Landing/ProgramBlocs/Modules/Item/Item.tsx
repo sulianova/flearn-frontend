39c71df658ac4a4711b177830e0ae846f0e89ce1
sulianova <ulianova.sofia@gmail.com>
2024-06-15 11:33:58 +0200

    review

--- src/pages/Course/Landing/ProgramBlocs/Modules/Item/Item.tsx
+++ src/pages/Course/Landing/ProgramBlocs/Modules/Item/Item.tsx
@@ -25,6 +25,7 @@ interface IProps {
 function Item(props: Readonly<IProps>) {
   const { index, module } = props;
   const isMobile = useIsMobile();
+  const alwaysOpened = index === 0;
   const [opened, setOpened] = useState(false);
 
   const tagsListNode = module.tags && module.tags.length && (
@@ -33,50 +34,49 @@ function Item(props: Readonly<IProps>) {
     </ul>
   );
 
+  const itemClasses = cx({
+    itemCard: true,
+    itemCardHoverable: Boolean(module.subsectionDescription && module.subsection && module.subsection.length),
+  });
+
   return (
     <div className={classes.item} style={{ '--items-in-row': 1 } as React.CSSProperties}>
-      <Animated.Scroll>
-        {(id, className) => (
-          <div className={cx({ itemCard: true }, className)} id={id}>
-            <div className={classes.bullet}>{index}</div>
-            <div className={classes.meta}>
-              <Text text={module.meta}/>
-              {index === 0 && (
-                <div className={classes.badge}>
-                  <Icon icon='Lightning'/>
-                  Бесплатно
-                </div>
-              )}
+      <div className={itemClasses}>
+        <div className={classes.bullet}>{index}</div>
+        <div className={classes.meta}>
+          <Text text={module.meta}/>
+          {index === 0 && (
+            <div className={classes.badge}>
+              <Icon icon='Lightning'/>
+              Бесплатно
             </div>
-            <div className={classes.title}>
-              <Text text={module.title}/>
-              <button
-                className={classes.hiddenButton}
-                type="button"
-                onClick={() => setOpened(o => !o)}
-              />
-            {!isMobile && tagsListNode}
-            </div>
-            {/* <div className={classes.description}>Познакомимся с упражнениями, которые помогают чувствовать себя увереннее в рисовании. Их можно делать для разминки перед рисованием или целенаправленно тренировать то, что плохо получается.</div> */}
-            <div className={cx({ slideDown: true, slideDownClosed: !opened })}>
-              {isMobile && tagsListNode}
-              <div className={classes.additionalInfo}>
-                {module.subsection && module.subsection.length && (
-                  <div className={classes.subsections}>
-                    {module.subsection.map(subsection => <Subsection subsection={subsection}/>)}
-                  </div>
-                )}
-                {module.subsectionDescription && (
-                  <div className={classes.projects}>
-                    <div className={classes.projectsDescription}>{module.subsectionDescription}</div>
-                  </div>
-                )}
+          )}
+        </div>
+        <div className={classes.title}>
+          <Text text={module.title}/>
+          <button
+            className={classes.hiddenButton}
+            type="button"
+            onClick={() => setOpened(o => !o)}
+          />
+        {!isMobile && tagsListNode}
+        </div>
+        {/* <div className={classes.description}>Познакомимся с упражнениями, которые помогают чувствовать себя увереннее в рисовании. Их можно делать для разминки перед рисованием или целенаправленно тренировать то, что плохо получается.</div> */}
+        <div className={cx({ slideDown: true, slideDownClosed: alwaysOpened ? !alwaysOpened : !opened })}>
+          {isMobile && tagsListNode}
+          {module.subsectionDescription && module.subsection && module.subsection.length && (
+            <div className={classes.additionalInfo}>
+              <div className={classes.subsections}>
+                {module.subsection.map((subsection, index) => <Subsection key={index} subsection={subsection}/>)}
+              </div>
+              <div className={classes.projects}>
+                <div className={classes.projectsDescription}>{module.subsectionDescription}</div>
               </div>
             </div>
-            {/* {props.content && Boolean(!Array.isArray(props.content) || props.content.length) && <div className={classes.listItemContent + ' s-text-21'}><Text text={props.content}/></div>} */}
-          </div>
-        )}
-      </Animated.Scroll>
+          )}
+        </div>
+        {/* {props.content && Boolean(!Array.isArray(props.content) || props.content.length) && <div className={classes.listItemContent + ' s-text-21'}><Text text={props.content}/></div>} */}
+      </div>
     </div>
   );
 }
