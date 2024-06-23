96de089268f09032a670d019b9925214f95924a8
Vladimir Fyodorov <vfyodorov@nes.ru>
2024-06-23 15:51:58 +0200

    fix svg

--- src/assets/images/Svg/PlusOpen.tsx
+++ src/assets/images/Svg/PlusOpen.tsx
@@ -1,7 +1,7 @@
 export function PlusOpen() {
   return (
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clip-path="url(#clip0_2821_393)">
+      <g clipPath="url(#clip0_2821_393)">
         <path d="M0 11.5H24"/>
         <path d="M12 24L12 0"/>
       </g>
