662bb04a54089ad69a49c1a138a6e1f275840711
Vladimir Fyodorov <vfyodorov@nes.ru>
2024-06-23 20:59:08 +0200

    fix MobileBtn

--- src/ui/Page/MobileBtn/MobileBtn.tsx
+++ src/ui/Page/MobileBtn/MobileBtn.tsx
@@ -17,39 +17,15 @@ interface IProps {
   course?: ICourseData
 }
 
-export default function MobileBtnContainer(props: IProps) {
+export default function MobileBtn({ course }: IProps) {
   const urlSection = useURLSection();
   const { theme } = frontendSettingsService.useFrontendSettings();
-  const changeTheme = () => frontendSettingsService.update({ theme: theme === 'light' ? 'dark' : 'light' });
-  if (urlSection === 'Course' && props.course) {
-    return <MobileBtnLanding course={props.course} changeTheme={changeTheme}/>
-  }
-
-  return (
-    <div className={classes.__}>
-      <div
-        className={classes.settings}
-        onClick={changeTheme}
-      >
-        <Icon icon='List'/>
-      </div>
-    </div>
-  );
-}
-
-interface IMobileBtnLandingProps {
-  course: ICourseData
-  changeTheme: () => void
-}
-
-function MobileBtnLanding({ course, changeTheme }: IMobileBtnLandingProps) {
-  const { id: courseId } = course;
-  const firstLesson = lessonService.useLessons({ courseId, topicOrder: 1, orderInTopic: 1 }).at(0);
+  const firstLesson = lessonService.useLessons({ courseId: course?.id, topicOrder: 1, orderInTopic: 1 }).at(0);
   const [popupVisible, setPopupVisible] = useState(false);
 
   return (
     <>
-      {popupVisible &&
+      {popupVisible && course &&
         <SignupToCoursePopup
           course={course}
           option={'OPTIMAL'}
@@ -57,26 +33,29 @@ function MobileBtnLanding({ course, changeTheme }: IMobileBtnLandingProps) {
         />
       }
       <div className={classes.__}>
-      {authService.isAuthenticated && firstLesson
-        ? (
-          <Link
-            className={classes.btnLink}
-            to={URLSections.Study.to({ courseId, lessonId: firstLesson.id })}
-          >
-            <div className={classes.text}>начать учиться</div>
-          </Link>
-        ) : (
-          <div
-            className={classes.btnLink}
-            onClick={() => setPopupVisible(true)}
-          >
-            <div className={classes.text}>начать учиться</div>
-          </div>
+        {urlSection === 'Course' && course && (
+          authService.isAuthenticated && firstLesson
+            ? (
+              <Link
+                className={classes.btnLink}
+                to={URLSections.Study.to({ courseId: course.id, lessonId: firstLesson.id })}
+              >
+                <div className={classes.text}>начать учиться</div>
+              </Link>
+            ) : (
+              <div
+                className={classes.btnLink}
+                onClick={() => setPopupVisible(true)}
+              >
+                <div className={classes.text}>начать учиться</div>
+              </div>
+            )
         )}
-        <div className={classes.settings} onClick={changeTheme}>
+        <div className={classes.settings} onClick={() => frontendSettingsService.update({ theme: theme === 'light' ? 'dark' : 'light' })}>
           <Icon icon='List'/>
         </div>
       </div>
     </>
   );
 }
+
