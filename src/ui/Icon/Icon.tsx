79dd72a8d4957c19d1e181d688570b688bb0b1e2
Vladimir Fyodorov <vfyodorov@nes.ru>
2024-07-04 15:12:29 +0200

    redu spinner

--- src/ui/Icon/Icon.tsx
+++ src/ui/Icon/Icon.tsx
@@ -1,51 +1,36 @@
-import classnames from 'classnames/bind';
 import React from 'react';
 
 import * as icons from 'assets/images/Svg';
 
-import classes from './Icon.module.scss';
-
 export default React.forwardRef(Icon);
 
-const cx = classnames.bind(classes);
-
 type TNativeProps =
-    & React.Attributes
-    & React.RefAttributes<SVGSVGElement>
-    & React.SVGAttributes<SVGSVGElement>;
+  & React.Attributes
+  & React.RefAttributes<SVGSVGElement>
+  & React.SVGAttributes<SVGSVGElement>;
 
 export type TIcon = keyof typeof icons;
 interface IPropsWithoutRef extends TNativeProps {
-    icon: TIcon | React.FunctionComponent<React.SVGProps<SVGSVGElement>>
-    rotate?: number
-    transition?: ['Flip', boolean]
-    sizeVariant?: 'Sm' | 'Md' | 'Lg'
+  icon: TIcon | React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  rotate?: number
+  height?: number
+  width?: number
 }
 
 export interface IIconProps extends IPropsWithoutRef, React.RefAttributes<SVGSVGElement> {}
 
 function Icon(props: Readonly<IPropsWithoutRef>, ref: React.Ref<SVGSVGElement>) {
-    const { icon, color, rotate, transition, sizeVariant, className, ...nativeProps } = props;
+  const { icon, rotate, height, width, ...nativeProps } = props;
 
-    const ConcreteIcon = typeof icon === 'string' ? icons[icon] : icon;
+  const ConcreteIcon = typeof icon === 'string' ? icons[icon] : icon;
 
-    const transitionType = transition?.[0];
-    const transitionEntered = transition?.[1];
-
-    return (
-        <div className={cx({
-            Icon: true,
-            [color!]: Boolean(color),
-            [className!]: Boolean(className),
-            [sizeVariant!]: Boolean(sizeVariant),
-            [transitionType!]: Boolean(transitionType),
-            TransitionEntered: Boolean(transitionEntered),
-        })}>
-            <ConcreteIcon
-                {...nativeProps}
-                style={{ transform: rotate ? `rotate(${rotate}deg)` : undefined, ...nativeProps.style }}
-                ref={ref}
-            />
-        </div>
-    );
+  return (
+    <ConcreteIcon
+      {...nativeProps}
+      height={height}
+      width={width}
+      style={{ transform: rotate ? `rotate(${rotate}deg)` : undefined, ...nativeProps.style }}
+      ref={ref}
+    />
+  );
 }
--- src/ui/Spinner/Spinner.tsx
+++ src/ui/Spinner/Spinner.tsx
@@ -1,23 +1,15 @@
-import classnames from 'classnames/bind';
-
 import Icon from '../Icon/Icon';
 
-import classes from './Spinner.module.scss';
-
-const cx = classnames.bind(classes);
-
 interface IProps {
-  className?: string
   variant?: 'local' | 'global'
 }
 
-export default function Spinner({ variant = 'local', className = '' }: Readonly<IProps>) {
-  const spinnerClasses = cx({
-    Base: true,
-    Local: variant === 'local',
-    Global: variant === 'global',
-    [className]: className !== '',
-  });
+export default function Spinner({ variant = 'local' }: Readonly<IProps>) {
+  const size = variant === 'local' ? 48 : 96;
 
-  return <Icon icon='Spinner' className={spinnerClasses} />;
+  return <Icon
+    icon='Spinner'
+    height={size}
+    width={size}
+  />;
 }
