70f2d7b4bd4f123824c13ea5f447f50fb7b48ffe
Vladimir Fyodorov <vfyodorov@nes.ru>
2024-07-04 15:27:11 +0200

    Task #292: landing gallery

--- src/pages/Course/ProgramBlocs/Gallery/Gallery.tsx
+++ src/pages/Course/ProgramBlocs/Gallery/Gallery.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import { useMemo, useState } from 'react';
 
 import type { TImageData } from 'services/course.service';
 import { formatI18nT } from 'shared';
@@ -18,18 +19,24 @@ interface IProps {
 }
 
 function Gallery({ teacherGallery }: IProps) {
+  const [index, setIndex] = useState(0);
+  const shiftedArray = useMemo(() =>
+    [...teacherGallery.slice(index), ...teacherGallery.slice(0, index)]
+  , [teacherGallery, index]);
+
   return (
   <>
     <h2 className={cx({ title: true })}>{t('title')}</h2>
     <div className={classes.__}>
       <div
         className={classes.backwardButton}
+        onClick={() => setIndex(index - 1 >= 0 ? index - 1 : teacherGallery.length - 1)}
       >
         <Icon icon='ArrowButton'/>
       </div>
       <div className={classes.commonFlowRow}>
         <div className={cx({ list: true })}>
-          {teacherGallery.map(({ imageSrc, imageAlt }, index) => (
+          {shiftedArray.map(({ imageSrc, imageAlt }, index) => (
             <div className={cx({ item: true })} key={index}>
               <Image src={imageSrc} alt={imageAlt}/>
             </div>
@@ -38,6 +45,7 @@ function Gallery({ teacherGallery }: IProps) {
       </div>
       <div
         className={classes.forwardButton}
+        onClick={() => setIndex(index + 1 < teacherGallery.length ? index + 1 : 0)}
       >
         <Icon icon='ArrowButton'/>
       </div>
