c883cc9551774b12c5ceab6ce74651b9442fd97c
sulianova <ulianova.sofia@gmail.com>
2024-07-09 14:20:44 +0200

    fix quiz

--- src/pages/Study/LessonSurvey/LessonSurvey.tsx
+++ src/pages/Study/LessonSurvey/LessonSurvey.tsx
@@ -10,98 +10,63 @@ const cx = classNames.bind(classes);
 
 function LessonSurvey() {
   return (
+
     <div className={classes.__}>
       <div className={classes.wrapper}>
-        <form className={classes.content}>
-          <div className={classes.quizHeader}>
-            <div className={classes.quizTitle}>Задание</div>
-            <div className={classes.quizDescription}>Рыбный текст для задания, а точнее вопроса, на который нужно дать ответ.</div>
+        <div className={classes.content}>
+          {/* <div className={classes.successScreen}>
+            <div className={classes.successScreenTitle}>Спасибо, мы записали ваш ответ!</div>
+          </div> */}
+          <div className={classes.survey}>
+            {/* <div className={classes.surveyHeader}>
+              <div className={classes.surveyTitle}>1/4. У&nbsp;вас есть опыт работы или учёбы по&nbsp;специальности «Иллюстратор»?</div>
+            </div>
+            <div className={classes.surveyAnswers}>
+              <label className={classes.surveyAnswer}>
+                <div className={cx({ radio: true, checked: true })}>
+                  <span className={classes.visuallyHidden}>
+                    <input type="radio" value="no"/>
+                  </span>
+                  <div className={classes.icon}>
+                    <Icon icon='Tick'/>
+                  </div>
+                </div>
+                <span>Нет, впервые знакомлюсь с профессией</span>
+              </label>
+              <label className={classes.surveyAnswer}>
+                <div className={cx({ checkbox: true, checkboxWithMovementAnimation: true, checked: true })}>
+                  <span className={classes.visuallyHidden}>
+                    <input type="checkbox" value="no"/>
+                  </span>
+                  <div className={classes.icon}>
+                    <Icon icon='Tick'/>
+                  </div>
+                </div>
+                <span>Да, уже в процессе изучения или работаю в этой сфере</span>
+              </label>
+            </div> */}
+            <div className={classes.surveyHeader}>
+              <div className={classes.surveySubtitle}>Хотим зафиксировать цель, с которой вы проходите вводную часть. А в конце спросим, получилось ли её достичь.</div>
+              <div className={classes.surveyDescription}>Выберите цель:</div>
+            </div>
+            <div className={classes.cards}>
+              <button className={cx({ card: true, active: false })}>
+                <div className={classes.cardTitle}>Пройти и оплатить</div>
+                <div className={classes.cardSubtitle}>Подготовиться к стартру курса</div>
+              </button>
+              <button className={cx({ card: true, active: false })}>
+                <div className={classes.cardTitle}>Попробовать формат</div>
+                <div className={classes.cardSubtitle}>Понять, понравится ли мне учеба в Flearn</div>
+              </button>
+              <button className={cx({ card: true, active: true })}>
+                <div className={classes.cardTitle}>Пройти только вводную часть</div>
+                <div className={classes.cardSubtitle}>Бесплатно научиться чему-нибудь новому</div>
+              </button>
+            </div>
           </div>
-          <fieldset className={classes.quizGroup} role={'radiogroup'}>
-            <label className={classes.choiceOption}>
-              <div className={cx({ radio: true, checked: false })}>
-                <span className={classes.visuallyHidden}>
-                  <input type="radio" value="no"/>
-                </span>
-                <div className={classes.icon}>
-                  <Icon icon='Tick'/>
-                </div>
-              </div>
-              <span>Нет, впервые знакомлюсь с профессией</span>
-            </label>
-            <label className={classes.choiceOption}>
-              <div className={cx({ radio: true, checked: true, shouldBeChecked: true })}>
-                <span className={classes.visuallyHidden}>
-                  <input type="radio" value="no"/>
-                </span>
-                <div className={classes.icon}>
-                  <Icon icon='SmallCross'/>
-                </div>
-              </div>
-              <span>Нет, впервые знакомлюсь с профессией</span>
-            </label>
-            <div className={classes.feedback}>Это подробное описвание ответа. Это подробное описвание ответа. Это подробное описвание ответа. Это подробное описвание ответа.</div>
-          </fieldset>
-          <button disabled={true} className={classes.submitButton}>Узнать ответ</button>
-        </form>
+          <button disabled={true} className={classes.submitButton}>Отправить</button>
+        </div>
       </div>
     </div>
-    // <div className={classes.__}>
-    //   <div className={classes.wrapper}>
-    //     <div className={classes.content}>
-    //       {/* <div className={classes.successScreen}>
-    //         <div className={classes.successScreenTitle}>Спасибо, мы записали ваш ответ!</div>
-    //       </div> */}
-    //       <div className={classes.survey}>
-    //         {/* <div className={classes.surveyHeader}>
-    //           <div className={classes.surveyTitle}>1/4. У&nbsp;вас есть опыт работы или учёбы по&nbsp;специальности «Иллюстратор»?</div>
-    //         </div>
-    //         <div className={classes.surveyAnswers}>
-    //           <label className={classes.surveyAnswer}>
-    //             <div className={cx({ radio: true, checked: true })}>
-    //               <span className={classes.visuallyHidden}>
-    //                 <input type="radio" value="no"/>
-    //               </span>
-    //               <div className={classes.icon}>
-    //                 <Icon icon='Tick'/>
-    //               </div>
-    //             </div>
-    //             <span>Нет, впервые знакомлюсь с профессией</span>
-    //           </label>
-    //           <label className={classes.surveyAnswer}>
-    //             <div className={cx({ checkbox: true, checkboxWithMovementAnimation: true, checked: true })}>
-    //               <span className={classes.visuallyHidden}>
-    //                 <input type="checkbox" value="no"/>
-    //               </span>
-    //               <div className={classes.icon}>
-    //                 <Icon icon='Tick'/>
-    //               </div>
-    //             </div>
-    //             <span>Да, уже в процессе изучения или работаю в этой сфере</span>
-    //           </label>
-    //         </div> */}
-    //         <div className={classes.surveyHeader}>
-    //           <div className={classes.surveySubtitle}>Хотим зафиксировать цель, с которой вы проходите вводную часть. А в конце спросим, получилось ли её достичь.</div>
-    //           <div className={classes.surveyDescription}>Выберите цель:</div>
-    //         </div>
-    //         <div className={classes.cards}>
-    //           <button className={cx({ card: true, active: false })}>
-    //             <div className={classes.cardTitle}>Пройти и оплатить</div>
-    //             <div className={classes.cardSubtitle}>Подготовиться к стартру курса</div>
-    //           </button>
-    //           <button className={cx({ card: true, active: false })}>
-    //             <div className={classes.cardTitle}>Попробовать формат</div>
-    //             <div className={classes.cardSubtitle}>Понять, понравится ли мне учеба в Flearn</div>
-    //           </button>
-    //           <button className={cx({ card: true, active: true })}>
-    //             <div className={classes.cardTitle}>Пройти только вводную часть</div>
-    //             <div className={classes.cardSubtitle}>Бесплатно научиться чему-нибудь новому</div>
-    //           </button>
-    //         </div>
-    //       </div>
-    //       <button disabled={true} className={classes.submitButton}>Отправить</button>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
--- src/ui/Article/Quiz/Quiz.tsx
+++ src/ui/Article/Quiz/Quiz.tsx
@@ -1,8 +1,13 @@
+import classNames from 'classnames/bind';
+
 import type { IArticleQuizBlock } from 'types';
 
 import UIText from 'ui/Text/Text';
+import Icon from 'ui/Icon/Icon';
 
 import classes from './Quiz.module.scss';
+
+const cx = classNames.bind(classes);
 
 export default function Quiz({ quiz, factoid }: Omit<IArticleQuizBlock, 'type'>) {
   if (!quiz) {
@@ -11,7 +16,42 @@ export default function Quiz({ quiz, factoid }: Omit<IArticleQuizBlock, 'type'>)
 
   return (
     <>
-      {/* {props.data.factoid && <p className={classes.factoid}> <UIText text={props.data.factoid}/> </p>} */}
+    <div className={classes.__}>
+      <div className={classes.wrapper}>
+        <form className={classes.content}>
+          <div className={classes.quizHeader}>
+            <div className={classes.quizTitle}>Задание</div>
+            <div className={classes.quizDescription}>Рыбный текст для задания, а точнее вопроса, на который нужно дать ответ.</div>
+          </div>
+          <fieldset className={classes.quizGroup} role={'radiogroup'}>
+            <label className={classes.choiceOption}>
+              <div className={cx({radio: true, checked: false, shouldBeChecked: false, shouldNotBeChecked: true })}>
+                <span className={classes.visuallyHidden}>
+                  <input type="radio" value="no"/>
+                </span>
+                <div className={classes.icon}>
+                  <Icon icon='Tick'/>
+                </div>
+              </div>
+              <span>Нет, впервые знакомлюсь с профессией</span>
+            </label>
+            <label className={classes.choiceOption}>
+              <div className={cx({ radio: true, checked: true, shouldBeChecked: true, shouldNotBeChecked: false })}>
+                <span className={classes.visuallyHidden}>
+                  <input type="radio" value="no"/>
+                </span>
+                <div className={classes.icon}>
+                  <Icon icon='SmallCross'/>
+                </div>
+              </div>
+              <span>Нет, впервые знакомлюсь с профессией</span>
+            </label>
+            <div className={classes.feedback}>Это подробное описвание ответа. Это подробное описвание ответа. Это подробное описвание ответа. Это подробное описвание ответа.</div>
+          </fieldset>
+          <button disabled={true} className={classes.submitButton}>Узнать ответ</button>
+        </form>
+      </div>
+    </div>
     </>
   );
 }
