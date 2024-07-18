1ec635e24ce5ebb3b40381ffb678538cfe96ab47
Vladimir Fyodorov <vfyodorov@nes.ru>
2024-07-18 13:23:57 +0200

    WIP

--- src/pages/Course/Course.tsx
+++ src/pages/Course/Course.tsx
@@ -11,7 +11,7 @@ export default function Course() {
   const course = courseService.useCurrentCourse();
 
   if (!course) {
-    return <Fallback.Pending/>;
+    return <Fallback.Pending headerVariant={EPageVariant.WEB}/>;
   }
 
   return (
