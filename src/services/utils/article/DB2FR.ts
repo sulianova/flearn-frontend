736ab6d1ba98b6242b3cfcb9837c5adaec4aaf51
Vladimir Fyodorov <vfyodorov@nes.ru>
2024-07-27 17:13:13 +0200

    WIP

--- src/services/utils/article/DB2FR.ts
+++ src/services/utils/article/DB2FR.ts
@@ -11,45 +11,42 @@ import type {
 
 import { addImageSrc, type TGetImageUrlProps } from '../shared';
 
-export async function articleDB2FR(contentDB: IArticleContentDB, getImageProps: Omit<TGetImageUrlProps, 'imageId'>) {
-  const contentFR: IArticleContent = await Promise.all(
+export async function articleDB2FR(contentDB: IArticleContentDB, getImageProps: Omit<TGetImageUrlProps, 'imageId'>): Promise<IArticleContent> {
+  return Promise.all(
     contentDB.map(c => {
       switch(c.type) {
         case 'image':
           return articleImageBlockDB2FR(c, getImageProps);
         case 'gallery':
           return articleGalleryBlockDB2FR(c, getImageProps);
+        case 'quiz':
+          return articleQuizeBlockDB2FR(c, getImageProps);
         default:
           return c;
       }
     }
   ));
-
-  return contentFR;
 }
 
-export async function articleImageBlockDB2FR(imageBlockDB: IArticleImageBlockDB, getImageProps: Omit<TGetImageUrlProps, 'imageId'>) {
-  const imageBlockFR: IArticleImageBlock = {
-    ...imageBlockDB,
-    imageData: await addImageSrc(imageBlockDB.imageData, { ...getImageProps, imageId: imageBlockDB.imageData.id }),
-  };
-  return imageBlockFR;
-}
-
-export async function articleGalleryBlockDB2FR(imageBlockDB: IArticleGalleryBlockDB, getImageProps: Omit<TGetImageUrlProps, 'imageId'>) {
-  const galleryBlockFR: IArticleGalleryBlock =
-  {
-    ...imageBlockDB,
-    images: await Promise.all(imageBlockDB.images.map(image => addImageSrc(image, { ...getImageProps, imageId: image.id }))),
-  };
-  return galleryBlockFR;
-}
-
-export async function articleQuizeBlockDB2FR(blockDB: IArticleQuizBlockDB, getImageProps: Omit<TGetImageUrlProps, 'imageId'>) {
-  const blockFR: IArticleQuizBlock =
-  {
+export async function articleImageBlockDB2FR(blockDB: IArticleImageBlockDB, getImageProps: Omit<TGetImageUrlProps, 'imageId'>): Promise<IArticleImageBlock> {
+  return {
     ...blockDB,
-    steps: await Promise.all(blockDB.steps.map(s => s.image ? ({ ...s, image: addImageSrc(s.image, { ...getImageProps, imageId: s.image.id })}) : s)),
+    imageData: await addImageSrc(blockDB.imageData, { ...getImageProps, imageId: blockDB.imageData.id }),
   };
-  return blockFR;
+}
+
+export async function articleGalleryBlockDB2FR(blockDB: IArticleGalleryBlockDB, getImageProps: Omit<TGetImageUrlProps, 'imageId'>): Promise<IArticleGalleryBlock> {
+  return {
+    ...blockDB,
+    images: await Promise.all(blockDB.images.map(image => addImageSrc(image, { ...getImageProps, imageId: image.id }))),
+  };
+}
+
+export async function articleQuizeBlockDB2FR(blockDB: IArticleQuizBlockDB, getImageProps: Omit<TGetImageUrlProps, 'imageId'>): Promise<IArticleQuizBlock> {
+  return {
+    ...blockDB,
+    steps: await Promise.all(blockDB.steps.map(
+      async s => ({ ...s, image: s.image ? await addImageSrc(s.image, { ...getImageProps, imageId: s.image.id }) : undefined })
+    )),
+  };
 }
--- src/services/utils/article/FR2DB.ts
+++ src/services/utils/article/FR2DB.ts
@@ -5,40 +5,45 @@ import type {
   IArticleGalleryBlockDB,
   IArticleImageBlock,
   IArticleImageBlockDB,
+  IArticleQuizBlock,
+  IArticleQuizBlockDB,
 } from 'types';
 
 import { removeImageSrc } from '../shared';
 
-export function articleFR2DB(contentDB: IArticleContent) {
-  const contentFR: IArticleContentDB = contentDB
+export function articleFR2DB(contentDB: IArticleContent): IArticleContentDB {
+  return contentDB
     .map(c => {
       switch(c.type) {
         case 'image':
           return articleImageBlockFR2DB(c);
         case 'gallery':
           return articleGalleryBlockFR2DB(c);
+        case 'quiz':
+          return articleQuizBlockFR2DB(c);
         default:
           return c;
       }
     });
-
-  return contentFR;
 }
 
-function articleImageBlockFR2DB(imageBlockDB: IArticleImageBlock) {
-  const imageBlockFR: IArticleImageBlockDB = {
-    ...imageBlockDB,
-    imageData: removeImageSrc(imageBlockDB.imageData),
+function articleImageBlockFR2DB(blockDB: IArticleImageBlock): IArticleImageBlockDB {
+  return {
+    ...blockDB,
+    imageData: removeImageSrc(blockDB.imageData),
   };
-  return imageBlockFR;
 }
 
-export function articleGalleryBlockFR2DB(galleryBlockFR: IArticleGalleryBlock) {
-  const galleryBlockDB: IArticleGalleryBlockDB =
-  {
-    ...galleryBlockFR,
-    images: galleryBlockFR.images.map(removeImageSrc),
+function articleGalleryBlockFR2DB(blockFR: IArticleGalleryBlock): IArticleGalleryBlockDB {
+  return {
+    ...blockFR,
+    images: blockFR.images.map(removeImageSrc),
   };
-  return galleryBlockDB;
 }
 
+function articleQuizBlockFR2DB(blockFR: IArticleQuizBlock): IArticleQuizBlockDB {
+  return {
+    ...blockFR,
+    steps: blockFR.steps.map(s => !s.image ? s : ({ ...s, image: removeImageSrc(s.image) })),
+  };
+}
