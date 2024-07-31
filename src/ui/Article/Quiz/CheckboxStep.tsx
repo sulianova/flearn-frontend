915b5d89772f31d6b990baf1ada2d3367dd93ce6
Vladimir Fyodorov <vfyodorov@nes.ru>
2024-07-31 20:39:59 +0200

    image modal quiz

--- src/ui/Article/Quiz/CheckboxStep.tsx
+++ src/ui/Article/Quiz/CheckboxStep.tsx
@@ -7,6 +7,7 @@ import { IQuizCheckboxStep } from 'types';
 import UIText from 'ui/Text/Text';
 import Icon from 'ui/Icon/Icon';
 import Img from 'ui/Img/Img';
+import ImageModal, { type IImage } from 'ui/ImageModal/ImageModal';
 
 import classes from './Quiz.module.scss';
 
@@ -28,93 +29,131 @@ export default function CheckboxStep(props: IProps) {
       : []
   );
   const [showAnswers, setShowAnswers] = useState(isInitialSolvedQuiz);
+  const [openedImage, setOpenedImage] = useState<IImage | null>(null);
 
   return (
-    <div className={cx({ wrapper: true, wrapperVisible: visible, submited: showAnswers })}>
-      <form className={classes.content}>
-        <div className={classes.quizHeader}>
-          {step.title && <div className={classes.quizTitle}><UIText text={step.title}/></div>}
-          {step.subtitle && <div className={classes.quizSubtitle}><UIText text={step.subtitle}/></div>}
-          {step.description && <div className={classes.quizDescription}><UIText text={step.description}/></div>}
-          {step.image && (<Img src={step.image.src} alt={step.image.alt} className={cx({ quizImage: true })}/>)}
-        </div>
-        <fieldset className={classes.quizGroup}>
-          {step.options.map((option, index) => (
-            <div
-              key={index}
-              className={cx({
-                quizGroupItem: true,
-                highlight: option.shouldBeSelected || selectedValueIndexes.includes(index),
-                shouldBeChecked: option.shouldBeSelected,
-                shouldNotBeChecked: !option.shouldBeSelected,
-              })}
-            >
-              <label
-                htmlFor={getId(index)}
-                className={classes.choiceOption}
+    <>
+      <div className={cx({ wrapper: true, wrapperVisible: visible, submited: showAnswers })}>
+        <form className={classes.content}>
+          <div className={classes.quizHeader}>
+            {step.title && <div className={classes.quizTitle}><UIText text={step.title}/></div>}
+            {step.subtitle && <div className={classes.quizSubtitle}><UIText text={step.subtitle}/></div>}
+            {step.description && <div className={classes.quizDescription}><UIText text={step.description}/></div>}
+            {step.image && (
+              <div
+                id={getId(step.image.id)}
+                onClick={() => {
+                  const element = document.getElementById(getId(step.image!.id));
+                  if (!element) {
+                    return;
+                  }
+                  const rect = element.getBoundingClientRect();
+                  setOpenedImage({
+                    data: step.image!,
+                    originalSize: {
+                      height: rect.height,
+                      width: rect.width,
+                    },
+                    originalPositioning: {
+                      top: rect.top,
+                      left: rect.left,
+                    },
+                  });
+                }}
               >
-                <div className={cx({
-                    checkbox: true,
-                    checked: selectedValueIndexes.includes(index),
-                  })}
+                <Img
+                  src={step.image.src}
+                  alt={step.image.alt}
+                  className={cx({ quizImage: true })}
+                />
+              </div>
+            )}
+          </div>
+          <fieldset className={classes.quizGroup}>
+            {step.options.map((option, index) => (
+              <div
+                key={index}
+                className={cx({
+                  quizGroupItem: true,
+                  highlight: option.shouldBeSelected || selectedValueIndexes.includes(index),
+                  shouldBeChecked: option.shouldBeSelected,
+                  shouldNotBeChecked: !option.shouldBeSelected,
+                })}
+              >
+                <label
+                  htmlFor={getId(index)}
+                  className={classes.choiceOption}
                 >
-                  <span className={classes.visuallyHidden}>
-                    <input
-                      type="checkbox"
-                      id={getId(index)}
-                      onChange={() => {
-                        if (showAnswers) {
-                          return;
-                        }
-                        if (!selectedValueIndexes.includes(index)) {
-                          setSelectedValueIndexes(v => [...v, index]);
-                        } else {
-                          const set = new Set(selectedValueIndexes);
-                          set.delete(index);
-                          setSelectedValueIndexes([...set]);
-                        }
-                      }}
-                    />
-                  </span>
-                  <div className={classes.icon}>
-                    <Icon icon='Tick'/>
+                  <div className={cx({
+                      checkbox: true,
+                      checked: selectedValueIndexes.includes(index),
+                    })}
+                  >
+                    <span className={classes.visuallyHidden}>
+                      <input
+                        type="checkbox"
+                        id={getId(index)}
+                        onChange={() => {
+                          if (showAnswers) {
+                            return;
+                          }
+                          if (!selectedValueIndexes.includes(index)) {
+                            setSelectedValueIndexes(v => [...v, index]);
+                          } else {
+                            const set = new Set(selectedValueIndexes);
+                            set.delete(index);
+                            setSelectedValueIndexes([...set]);
+                          }
+                        }}
+                      />
+                    </span>
+                    <div className={classes.icon}>
+                      <Icon icon='Tick'/>
+                    </div>
                   </div>
-                </div>
-                <span>
-                  <UIText text={option.value}/>
-                </span>
-              </label>
-              {showAnswers && option.shouldBeSelected && option.positiveExplanation && selectedValueIndexes.includes(index) && (
-                <div className={classes.feedback}>
-                  <UIText text={option.positiveExplanation}/>
-                </div>
-              )}
-              {showAnswers && option.shouldBeSelected && option.negativeExplanation && !selectedValueIndexes.includes(index) && (
-                <div className={classes.feedback}>
-                  <UIText text={option.negativeExplanation}/>
-                </div>
-              )}
-              {showAnswers && !option.shouldBeSelected && option.negativeExplanation && selectedValueIndexes.includes(index) && (
-                <div className={classes.feedback}>
-                  <UIText text={option.negativeExplanation}/>
-                </div>
-              )}
-            </div>
-          ))}
-        </fieldset>
-        {!showAnswers && (
-          <button
-            disabled={selectedValueIndexes.length === 0}
-            className={classes.submitButton}
-            onClick={() => {
-              setShowAnswers(true);
-              onSubmit();
-            }}
-          >
-            Узнать ответ
-          </button>
-        )}
-      </form>
-    </div>
+                  <span>
+                    <UIText text={option.value}/>
+                  </span>
+                </label>
+                {showAnswers && option.shouldBeSelected && option.positiveExplanation && selectedValueIndexes.includes(index) && (
+                  <div className={classes.feedback}>
+                    <UIText text={option.positiveExplanation}/>
+                  </div>
+                )}
+                {showAnswers && option.shouldBeSelected && option.negativeExplanation && !selectedValueIndexes.includes(index) && (
+                  <div className={classes.feedback}>
+                    <UIText text={option.negativeExplanation}/>
+                  </div>
+                )}
+                {showAnswers && !option.shouldBeSelected && option.negativeExplanation && selectedValueIndexes.includes(index) && (
+                  <div className={classes.feedback}>
+                    <UIText text={option.negativeExplanation}/>
+                  </div>
+                )}
+              </div>
+            ))}
+          </fieldset>
+          {!showAnswers && (
+            <button
+              disabled={selectedValueIndexes.length === 0}
+              className={classes.submitButton}
+              onClick={() => {
+                setShowAnswers(true);
+                onSubmit();
+              }}
+            >
+              Узнать ответ
+            </button>
+          )}
+        </form>
+      </div>
+      {openedImage && (
+        <ImageModal
+          variant='IMAGE'
+          image={openedImage}
+          onClose={() => setOpenedImage(null)}
+        />
+      )}
+    </>
   );
 }
--- src/ui/Article/Quiz/RadioStep.tsx
+++ src/ui/Article/Quiz/RadioStep.tsx
@@ -7,6 +7,7 @@ import { IQuizRadioStep } from 'types';
 import UIText from 'ui/Text/Text';
 import Icon from 'ui/Icon/Icon';
 import Img from 'ui/Img/Img';
+import ImageModal, { type IImage } from 'ui/ImageModal/ImageModal';
 
 import classes from './Quiz.module.scss';
 
@@ -26,87 +27,125 @@ export default function RadioStep(props: IProps) {
     props.isInitialSolvedQuiz ? step.options.findIndex(o => o.shouldBeSelected) : undefined
   );
   const [showAnswers, setShowAnswers] = useState(props.isInitialSolvedQuiz);
+  const [openedImage, setOpenedImage] = useState<IImage | null>(null);
 
   return (
-    <div className={cx({ wrapper: true, wrapperVisible: visible, submited: showAnswers })}>
-      <form className={classes.content}>
-        <div className={classes.quizHeader}>
-          {step.title && <div className={classes.quizTitle}><UIText text={step.title}/></div>}
-          {step.subtitle && <div className={classes.quizSubtitle}><UIText text={step.subtitle}/></div>}
-          {step.description && <div className={classes.quizDescription}><UIText text={step.description}/></div>}
-          {step.image && (<Img src={step.image.src} alt={step.image.alt} className={cx({ quizImage: true })}/>)}
-        </div>
-        <fieldset className={classes.quizGroup} role='radiogroup'>
-          {step.options.map((option, index) => (
-            <div
-              key={index}
-              className={cx({
-                quizGroupItem: true,
-                highlight: option.shouldBeSelected || selectedValueIndex === index,
-                shouldBeChecked: option.shouldBeSelected,
-                shouldNotBeChecked: !option.shouldBeSelected,
-              })}
-            >
-              <label
-                htmlFor={getId(index)}
-                className={classes.choiceOption}
-              >
-                <div className={cx({
-                    radio: true,
-                    checked: selectedValueIndex === index,
-                  })}
+    <>
+      <div className={cx({ wrapper: true, wrapperVisible: visible, submited: showAnswers })}>
+        <form className={classes.content}>
+          <div className={classes.quizHeader}>
+            {step.title && <div className={classes.quizTitle}><UIText text={step.title}/></div>}
+            {step.subtitle && <div className={classes.quizSubtitle}><UIText text={step.subtitle}/></div>}
+            {step.description && <div className={classes.quizDescription}><UIText text={step.description}/></div>}
+            {step.image && (
+                <div
+                  id={getId(step.image.id)}
+                  onClick={() => {
+                    const element = document.getElementById(getId(step.image!.id));
+                    if (!element) {
+                      return;
+                    }
+                    const rect = element.getBoundingClientRect();
+                    setOpenedImage({
+                      data: step.image!,
+                      originalSize: {
+                        height: rect.height,
+                        width: rect.width,
+                      },
+                      originalPositioning: {
+                        top: rect.top,
+                        left: rect.left,
+                      },
+                    });
+                  }}
                 >
-                  <span className={classes.visuallyHidden}>
-                    <input
-                      type="radio"
-                      id={getId(index)}
-                      onClick={() => {
-                        if (showAnswers) {
-                          return;
-                        }
-                        setSelectedValueIndex(index);
-                      }}
-                    />
-                  </span>
-                  <div className={classes.icon}>
-                    <Icon icon='Tick'/>
+                  <Img
+                    src={step.image.src}
+                    alt={step.image.alt}
+                    className={cx({ quizImage: true })}
+                  />
+                </div>
+              )}
+          </div>
+          <fieldset className={classes.quizGroup} role='radiogroup'>
+            {step.options.map((option, index) => (
+              <div
+                key={index}
+                className={cx({
+                  quizGroupItem: true,
+                  highlight: option.shouldBeSelected || selectedValueIndex === index,
+                  shouldBeChecked: option.shouldBeSelected,
+                  shouldNotBeChecked: !option.shouldBeSelected,
+                })}
+              >
+                <label
+                  htmlFor={getId(index)}
+                  className={classes.choiceOption}
+                >
+                  <div className={cx({
+                      radio: true,
+                      checked: selectedValueIndex === index,
+                    })}
+                  >
+                    <span className={classes.visuallyHidden}>
+                      <input
+                        type="radio"
+                        id={getId(index)}
+                        onClick={() => {
+                          if (showAnswers) {
+                            return;
+                          }
+                          setSelectedValueIndex(index);
+                        }}
+                      />
+                    </span>
+                    <div className={classes.icon}>
+                      <Icon icon='Tick'/>
+                    </div>
                   </div>
-                </div>
-                <span>
-                  <UIText text={option.value}/>
-                </span>
-              </label>
-              {showAnswers && option.shouldBeSelected && option.positiveExplanation && selectedValueIndex === index && (
-                <div className={classes.feedbackShouldBeChecked}>
-                  <UIText text={option.positiveExplanation}/>
-                </div>
-              )}
-              {showAnswers && option.shouldBeSelected && option.negativeExplanation && selectedValueIndex !== index && (
-                <div className={classes.feedbackShouldBeChecked}>
-                  <UIText text={option.negativeExplanation}/>
-                </div>
-              )}
-              {showAnswers && !option.shouldBeSelected && option.negativeExplanation && selectedValueIndex === index && (
-                <div className={classes.feedbackShouldNotBeChecked}>
-                  <UIText text={option.negativeExplanation}/>
-                </div>
-              )}
-            </div>
-          ))}
-        </fieldset>
-        {!showAnswers && (
-          <button
-            disabled={selectedValueIndex === undefined}
-            className={classes.submitButton}
-            onClick={() => {
-              setShowAnswers(true);
-              onSubmit();
-            }}
-          >
-            Узнать ответ
-          </button>
-        )}
-      </form>
-    </div>
+                  <span>
+                    <UIText text={option.value}/>
+                  </span>
+                </label>
+                {showAnswers && option.shouldBeSelected && option.positiveExplanation && selectedValueIndex === index && (
+                  <div className={classes.feedbackShouldBeChecked}>
+                    <UIText text={option.positiveExplanation}/>
+                  </div>
+                )}
+                {showAnswers && option.shouldBeSelected && option.negativeExplanation && selectedValueIndex !== index && (
+                  <div className={classes.feedbackShouldBeChecked}>
+                    <UIText text={option.negativeExplanation}/>
+                  </div>
+                )}
+                {showAnswers && !option.shouldBeSelected && option.negativeExplanation && selectedValueIndex === index && (
+                  <div className={classes.feedbackShouldNotBeChecked}>
+                    <UIText text={option.negativeExplanation}/>
+                  </div>
+                )}
+              </div>
+            ))}
+          </fieldset>
+          {!showAnswers && (
+            <button
+              disabled={selectedValueIndex === undefined}
+              className={classes.submitButton}
+              onClick={() => {
+                setShowAnswers(true);
+                onSubmit();
+              }}
+            >
+              Узнать ответ
+            </button>
+          )}
+        </form>
+      </div>
+      {openedImage && (
+        <ImageModal
+          variant='IMAGE'
+          image={openedImage}
+          onClose={() => setOpenedImage(null)}
+        />
+      )}
+    </>
   );
 }
