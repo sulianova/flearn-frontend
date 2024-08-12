f12e4623c611b6f7e08c6cbb9986867a2e2ed354
sulianova <ulianova.sofia@gmail.com>
2024-08-12 12:29:31 +0200

    add svg

--- src/assets/images/Svg/Start.tsx
+++ src/assets/images/Svg/Start.tsx
@@ -1,5 +1,5 @@
 export function Start() {
   return (
-<svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 14"><polygon points="10.5 7 1.5 1.77 1.5 12.23 10.5 7" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/><rect x="0" y="0" width="12" height="14" fill="none" stroke-width="0"/></svg>
+<svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><polygon points="19.5 12 4.5 2.5 4.5 21.5 19.5 12" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/><rect width="24" height="24" fill="none" stroke-width="0"/></svg>
   );
 }
--- src/assets/images/Svg/Time.tsx
+++ src/assets/images/Svg/Time.tsx
@@ -1,5 +1,5 @@
 export function Time() {
   return (
-    <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><polyline points="8 4.9 8 8 10.3 10.48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/><path d="M8,1.5c3.58,0,6.5,2.92,6.5,6.5s-2.92,6.5-6.5,6.5S1.5,11.58,1.5,8,4.42,1.5,8,1.5M8,0C3.58,0,0,3.58,0,8s3.58,8,8,8,8-3.58,8-8S12.42,0,8,0h0Z" fill="currentColor" stroke-width="0"/><rect x="0" y="0" width="16" height="16" fill="none" stroke-width="0"/></svg>
+    <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><polyline points="12 6.01 12 12 16.38 16.58" fill="none" stroke="#5f6368" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/><path d="M12,2c5.51,0,10,4.49,10,10s-4.39,10-10,10S2,17.61,2,12,6.39,2,12,2M12,0C5.25,0,0,5.4,0,12s5.25,12,12,12,12-5.4,12-12S18.6,0,12,0h0Z" fill="currentColor" stroke-width="0"/><rect width="24" height="24" fill="none" stroke-width="0"/></svg>
   );
 }
