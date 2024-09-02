d7b4b1e56ed03e01ed3b437beb0e299144ae8bf7
sulianova <ulianova.sofia@gmail.com>
2024-09-02 12:39:43 +0200

    add Course Page

---
+++ src/pages/Profile/CoursePage/CoursePage.tsx
@@ -0,0 +1,178 @@
+import classnames from 'classnames/bind';
+import { useMemo, useState } from 'react';
+
+import { formatI18nT, i18n } from 'shared';
+
+import { type IUserData, userService } from 'services/user.service';
+import { type ILessonData, lessonService } from 'services/lesson.service';
+import { type ICourseData, courseService } from 'services/course.service';
+import { type TAccess, userAccessService } from 'services/userAccess.service';
+
+import Icon from 'ui/Icon/Icon';
+
+import classes from './CoursePage.module.scss'
+
+const cx = classnames.bind(classes);
+
+interface IGroup extends Pick<ILessonData, 'topic' | 'topicOrder' | 'topicIcon'> {  
+  isFree: boolean
+  solved: boolean
+  lessons: (ILessonData & { solved: boolean, canBeAccessed: boolean })[]
+}
+
+interface IProps {
+  authedUser: IUserData
+  currentCourse: ICourseData
+  courseLessons: Array<ILessonData & { solved: boolean, canBeAccessed: boolean }>
+  currentCourseAccess: TAccess
+}
+
+export default function CoursePage(props: IProps) {
+  const { authedUser, currentCourse, courseLessons, currentCourseAccess } = props;
+  const [openedTopic, setOpenedTopic] = useState<string | null>(null);
+
+  const groupes: IGroup[] = useMemo(() => {
+    const getKey = (topic: string, topicOrder: number) => `${topic}-${topicOrder}`;
+    return [...courseLessons
+      .reduce((acc, lessonData) => {
+        const key = getKey(lessonData.topic, lessonData.topicOrder);
+
+        if (!acc.has(key)) {
+          acc.set(key, {
+            topic: lessonData.topic,
+            topicOrder: lessonData.topicOrder,
+            topicIcon: lessonData.topicIcon,
+            isFree: lessonData.isFree,
+            solved: lessonData.solved,
+            lessons: [lessonData],
+          })
+        } else {
+          const group = acc.get(key)!;
+          group.isFree = group.isFree && lessonData.isFree;
+          group.solved = group.solved && lessonData.solved;
+          group.lessons.push(lessonData);
+          group.lessons.sort((a, b) => a.orderInTopic - b.orderInTopic);
+        }
+
+        return acc;
+      }, new Map() as Map<string, IGroup>)
+      .values()]
+      .sort((a, b) => a.topicOrder - b.topicOrder);
+  }, [courseLessons]);
+
+  const firstNotSolvedLesson = courseLessons.find(l => !l.solved);
+  const freeGroupes = groupes.filter(g => g.isFree);
+  const payableGroupes = groupes.filter(g => !g.isFree);
+
+  return (
+    (currentCourseAccess !== 'FREE' || authedUser.role === 'support') ? (
+      <div className={classes.coursePage}>
+        <div className={classes.main}>
+          <div className={classes.level}>
+            <div className={classes.levelTitle}>Модули</div>
+              <div className={classes.wrapper}>
+                {[...freeGroupes, ...payableGroupes].map((group, index) => (
+                  <TopicCard
+                    key={index}
+                    group={group}
+                    setOpenedTopic={setOpenedTopic}
+                  />
+                ))}
+            </div>
+          </div>
+        </div>
+        <aside className={classes.asideWrapper}>
+          <div className={classes.aside}>
+            <div className={classes.asideSection}>
+              <div className={classes.sectionSubtitle}>Ключевые навыки</div>
+              <div className={classes.chipsSmall}>
+                <div className={classes.chipSmall}>Выделение главного</div>
+              </div>
+            </div>
+          </div>
+        </aside>
+      </div>
+    ) : (
+      <div className={classes.coursePage}>
+        <div className={classes.main}>
+          <div className={classes.level}>
+            <div className={classes.levelTitle}>Доступно сейчас и бесплатно</div>
+              <div className={classes.wrapper}>
+                {freeGroupes.map((group, index) => (
+                  <TopicCard
+                    key={index}
+                    group={group}
+                    setOpenedTopic={setOpenedTopic}
+                  />
+                ))}
+            </div>
+          </div>
+          <div className={classes.level}>
+            <div className={classes.levelTitle}>Будет доступно после оплаты</div>
+              <div className={classes.wrapper}>
+                {payableGroupes.map((group, index) => (
+                  <TopicCard
+                    key={index}
+                    group={group}
+                    setOpenedTopic={setOpenedTopic}
+                  />
+                ))}
+            </div>
+          </div>
+        </div>
+        <aside className={classes.asideWrapper}>
+          <div className={classes.aside}>
+            <div className={classes.asideSection}>
+              <div className={classes.sectionSubtitle}>Ключевые навыки</div>
+              <div className={classes.chipsSmall}>
+                <div className={classes.chipSmall}>Выделение главного</div>
+              </div>
+            </div>
+          </div>
+        </aside>
+      </div>
+    )
+  );
+}
+
+function TopicCard(props: { group: IGroup, setOpenedTopic: (topic: string) => void }) {
+  const { group, setOpenedTopic } = props;
+  const totalDurationMinutes = group.lessons.reduce((acc, l) => acc + durationToMinutes(l.duration), 0);
+  const totalDurationStr = totalDurationMinutes >= 60
+    ? `${Math.round(totalDurationMinutes / 6) / 10} ч`
+    : `${Math.round(totalDurationMinutes)} мин`;
+  return (
+    <div className={classes.itemWrapper} onClick={() => setOpenedTopic(group.topic)}>
+      <div className={cx({ item: true, featured: false })}>
+        <div className={classes.imageWrapper}>
+          <div className={classes.image}>
+            <Icon icon={group.topicIcon}/>
+          </div>
+        </div>
+        <div className={classes.itemBody}>
+          <div className={classes.itemBodyContainer}>
+            <div className={classes.titleContainer}>
+              <h2 className={classes.title}>
+                {group.topic}
+              </h2>
+            </div>
+          </div>
+          <div className={classes.info}>
+            <div className={classes.infoMain}>
+              <span className={classes.infoItem}>{i18n.t('lesson.p', { count: group.lessons.length })}</span>
+              <span className={classes.infoItem}>{`${totalDurationStr}  `}</span>
+            </div>
+          </div>
+        </div>
+        <div className={cx({ itemStatus: true, solved: group.solved })}>
+          <Icon icon='Tick'/>
+        </div>
+        <div className={classes.itemPopover}>Учиться</div>
+      </div>
+    </div>
+  );
+}
+
+function durationToMinutes(duration: { unit: 'minutes' | 'hours', value: number }) {
+  return (duration.unit === 'hours' ? 60 : 1) * duration.value;
+}
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,4 +1,3 @@
-import classnames from 'classnames/bind';
 import { useMemo, useState } from 'react';
 
 import { formatI18nT, i18n } from 'shared';
@@ -10,6 +9,7 @@ import { type ICourseData, courseService } from 'services/course.service';
 import { type TAccess, userAccessService } from 'services/userAccess.service';
 
 import Description from './Description/Description';
+import CoursePage from './CoursePage/CoursePage';
 import SocialValidation from './SocialValidation/SocialValidation';
 import Catalogue from './Catalogue/Catalogue';
 import BuyPopup from 'components/BuyPopup/BuyPopup';
@@ -19,10 +19,7 @@ import Page, { EPageVariant } from 'ui/Page/Page';
 import Fallback from 'ui/Fallback';
 import Icon from 'ui/Icon/Icon';
 
-import classesCoursePage from './CoursePage.module.scss';
 import classes from './Profile.module.scss';
-
-const cx = classnames.bind(classesCoursePage);
 
 interface IGroup extends Pick<ILessonData, 'topic' | 'topicOrder' | 'topicIcon'> {  
   isFree: boolean
@@ -65,38 +62,7 @@ function Profile(props: IProps) {
   const [openedTopic, setOpenedTopic] = useState<string | null>(null);
   const [buyCoursePopupIsOpened, setBuyCoursePopupIsOpened] = useState(false);
 
-  const groupes: IGroup[] = useMemo(() => {
-    const getKey = (topic: string, topicOrder: number) => `${topic}-${topicOrder}`;
-    return [...courseLessons
-      .reduce((acc, lessonData) => {
-        const key = getKey(lessonData.topic, lessonData.topicOrder);
-
-        if (!acc.has(key)) {
-          acc.set(key, {
-            topic: lessonData.topic,
-            topicOrder: lessonData.topicOrder,
-            topicIcon: lessonData.topicIcon,
-            isFree: lessonData.isFree,
-            solved: lessonData.solved,
-            lessons: [lessonData],
-          })
-        } else {
-          const group = acc.get(key)!;
-          group.isFree = group.isFree && lessonData.isFree;
-          group.solved = group.solved && lessonData.solved;
-          group.lessons.push(lessonData);
-          group.lessons.sort((a, b) => a.orderInTopic - b.orderInTopic);
-        }
-
-        return acc;
-      }, new Map() as Map<string, IGroup>)
-      .values()]
-      .sort((a, b) => a.topicOrder - b.topicOrder);
-  }, [courseLessons]);
-
   const firstNotSolvedLesson = courseLessons.find(l => !l.solved);
-  const freeGroupes = groupes.filter(g => g.isFree);
-  const payableGroupes = groupes.filter(g => !g.isFree);
   const {
     description,
     feedbacks
@@ -111,6 +77,7 @@ function Profile(props: IProps) {
   const onNotAuthedClick = () => setPopupVisible(true);
 
   const blocks: JSX.Element[] = [
+    <CoursePage authedUser={authedUser} currentCourse={currentCourse} courseLessons={courseLessons} currentCourseAccess={currentCourseAccess}/>,
     description && <Description type={currentCourse.type} description={description}/>,
     feedbacks && feedbacks.length && <SocialValidation feedbacks={feedbacks}/>,
     <Catalogue linkToFreeCourse={linkToFreeCourse} onNotAuthedClick={onNotAuthedClick}/>,
@@ -173,76 +140,9 @@ function Profile(props: IProps) {
                       </div>
                     </div>
               ) : (
-                <div>Реклама</div>
+                <></>
               )}
             </div>
-            {(currentCourseAccess !== 'FREE' || authedUser.role === 'support') ? (
-              <div className={classesCoursePage.coursePage}>
-                <div className={classesCoursePage.main}>
-                  <div className={classesCoursePage.level}>
-                    <div className={classesCoursePage.levelTitle}>Модули</div>
-                      <div className={classesCoursePage.wrapper}>
-                        {[...freeGroupes, ...payableGroupes].map((group, index) => (
-                          <TopicCard
-                            key={index}
-                            group={group}
-                            setOpenedTopic={setOpenedTopic}
-                          />
-                        ))}
-                    </div>
-                  </div>
-                </div>
-                <aside className={classesCoursePage.asideWrapper}>
-                  <div className={classesCoursePage.aside}>
-                    <div className={classesCoursePage.asideSection}>
-                      <div className={classesCoursePage.sectionSubtitle}>Ключевые навыки</div>
-                      <div className={classesCoursePage.chipsSmall}>
-                        <div className={classesCoursePage.chipSmall}>Выделение главного</div>
-                      </div>
-                    </div>
-                  </div>
-                </aside>
-              </div>
-            ) : (
-              <div className={classesCoursePage.coursePage}>
-                <div className={classesCoursePage.main}>
-                  <div className={classesCoursePage.level}>
-                    <div className={classesCoursePage.levelTitle}>Доступно сейчас и бесплатно</div>
-                      <div className={classesCoursePage.wrapper}>
-                        {freeGroupes.map((group, index) => (
-                          <TopicCard
-                            key={index}
-                            group={group}
-                            setOpenedTopic={setOpenedTopic}
-                          />
-                        ))}
-                    </div>
-                  </div>
-                  <div className={classesCoursePage.level}>
-                    <div className={classesCoursePage.levelTitle}>Будет доступно после оплаты</div>
-                      <div className={classesCoursePage.wrapper}>
-                        {payableGroupes.map((group, index) => (
-                          <TopicCard
-                            key={index}
-                            group={group}
-                            setOpenedTopic={setOpenedTopic}
-                          />
-                        ))}
-                    </div>
-                  </div>
-                </div>
-                <aside className={classesCoursePage.asideWrapper}>
-                  <div className={classesCoursePage.aside}>
-                    <div className={classesCoursePage.asideSection}>
-                      <div className={classesCoursePage.sectionSubtitle}>Ключевые навыки</div>
-                      <div className={classesCoursePage.chipsSmall}>
-                        <div className={classesCoursePage.chipSmall}>Выделение главного</div>
-                      </div>
-                    </div>
-                  </div>
-                </aside>
-              </div>
-            )}
             {blocks.map((block, index) => (
               <div key={index} className={classes.section}>
                 {block}
@@ -269,44 +169,3 @@ function Profile(props: IProps) {
   );
 }
 
-function durationToMinutes(duration: { unit: 'minutes' | 'hours', value: number }) {
-  return (duration.unit === 'hours' ? 60 : 1) * duration.value;
-}
-
-function TopicCard(props: { group: IGroup, setOpenedTopic: (topic: string) => void }) {
-  const { group, setOpenedTopic } = props;
-  const totalDurationMinutes = group.lessons.reduce((acc, l) => acc + durationToMinutes(l.duration), 0);
-  const totalDurationStr = totalDurationMinutes >= 60
-    ? `${Math.round(totalDurationMinutes / 6) / 10} ч`
-    : `${Math.round(totalDurationMinutes)} мин`;
-  return (
-    <div className={classesCoursePage.itemWrapper} onClick={() => setOpenedTopic(group.topic)}>
-      <div className={cx({ item: true, featured: false })}>
-        <div className={classesCoursePage.imageWrapper}>
-          <div className={classesCoursePage.image}>
-            <Icon icon={group.topicIcon}/>
-          </div>
-        </div>
-        <div className={classesCoursePage.itemBody}>
-          <div className={classesCoursePage.itemBodyContainer}>
-            <div className={classesCoursePage.titleContainer}>
-              <h2 className={classesCoursePage.title}>
-                {group.topic}
-              </h2>
-            </div>
-          </div>
-          <div className={classesCoursePage.info}>
-            <div className={classesCoursePage.infoMain}>
-              <span className={classesCoursePage.infoItem}>{i18n.t('lesson.p', { count: group.lessons.length })}</span>
-              <span className={classesCoursePage.infoItem}>{`${totalDurationStr}  `}</span>
-            </div>
-          </div>
-        </div>
-        <div className={cx({ itemStatus: true, solved: group.solved })}>
-          <Icon icon='Tick'/>
-        </div>
-        <div className={classesCoursePage.itemPopover}>Учиться</div>
-      </div>
-    </div>
-  );
-}
